ec0f10671f06480e1482583fd04d9b7a8bac3687
Batool261 <batoolhamouda261@gmail.com>
2024-06-23 14:49:30 +0300

    Add files via upload

---
+++ iwp201-2/iwp201/html/newFile.js
@@ -0,0 +1,17 @@
+$("#submitButton").click(function () {
+    var nationalId = $("#nationalId").val();
+    var dateOfBirth = $("#dateOfBirth").val();
+    var mobile;
+});
+
+
+
+
+$(document).ready(function() {
+    $('#btn1').click(function(){
+        $('#info1').
+    })
+
+
+    
+})
